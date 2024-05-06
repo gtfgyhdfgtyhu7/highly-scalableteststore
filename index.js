450c558d2681fe521e2f5fb94e0ebd8af21bb347
gtfgyhdfgtyhu7 <gs1ajsjsjsi@hotmail.com>
2024-05-06 15:50:48 +0700

    aptos

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-const fibonacciRecursive = (n) => {
-  if (n <= 1) {
-    return n;
+const binarySearch = (arr, target) => {
+  let left = 0;
+  let right = arr.length - 1;
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+    if (arr[mid] === target) {
+      return mid;
+    } else if (arr[mid] < target) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
   }
-  return fibonacciRecursive(n - 1) + fibonacciRecursive(n - 2);
+  return -1;
 };
